9cb79a871d1d1a7f639d431c06df595f80ead135
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-10 18:00:31 -0700

    Touched up and added docstrings for functions

--- ratings.py
+++ ratings.py
@@ -3,10 +3,10 @@ scores_file = open("scores.txt")
 
 
 
-
 def read_ratings(filename):
 
-    """ Reads the ratings from the file"""
+    """ Creates an empty dictionary called ratings, takes each line
+    from the text file one by one, and adds it to the empty dictionary. """
 
     ratings = {}
 
@@ -19,23 +19,28 @@ def read_ratings(filename):
     return ratings #returns dictionary
 
 
+def add_name_score(ratings):
+    """Takes a name and rating as input from the user, and adds that as a new item
+    to the dictionary 'ratings' from read_ratings() function"""
+
+    name = input("Enter a restaurant name: ")
+    score = input("Enter a rating: ")
+
+    ratings[name] = score # Adds a new key and value to the dictionary
+
+
 def sort_by_name(ratings):       
+    """Sorts each rating aphabetically by name and prints out each set of restaurants
+    and their corresponding ratings. """
+
     sorted_by_name = sorted(ratings.items())
     for eachname in sorted_by_name:
         print("{} is rated a {}.".format(eachname[0], eachname[1]))
 # read_ratings(scores_file)
 
 
-def add_name_score(ratings):
-
-    name = input("Enter a restaurant name: ")
-    score = input("Enter a rating: ")
-
-    ratings[name] = score
-
-
 ratings = read_ratings(scores_file)
 
-print(add_name_score(ratings))
+add_name_score(ratings)
 sort_by_name(ratings)
 # put your code here
